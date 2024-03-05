f1f599ca0c8852127ce9ca4fe526298280db3072
khakimovSamdu <allamurodxakimov@gmail.com>
2024-03-05 21:34:16 +0500

    telefon shope

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-w_9989#)p(rd_gl!cd(_v7f7nh0^-sax5izk%mr4_g5ac=2+c!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
